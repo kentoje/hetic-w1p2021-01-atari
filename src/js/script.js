03aacec1a72ff18871e8281c6e05d98afef9df04
jean Tostivint <jean.tostivint@gmail.com>
2018-12-19 00:41:50 +0100

    js fin de l'algorithme

--- src/js/script.js
+++ src/js/script.js
@@ -5,11 +5,10 @@
 */
 
 /*
-* Demander setInterval
+* Demander setInterval -> s'arrêter et gagner de la vitesse
+* Demander Positionnement temps réel
+* Demander pour BAD and GOOD
 */
-
-
-
 
 /* 
 * Variable
@@ -18,9 +17,11 @@
 let initial; // Value Initial Game
 let position_minimum  = 500; // Initial Position
 let position_final = 600; // Finak Position
-let gift_array = ['gift_0', 'gift_1', 'gift_2', 'gift_3']; // Array Gifts
-let fireplace_array = ['fireplace_0', 'fireplace_2', 'fireplace_3', 'fireplace_4']; // Array Spawns
+let gift_statut = ['good', 'bad']; // Define if the gift is good or bad
+let gift_array = [`gift_0 ${gift_statut[0]}`, `gift_1 ${gift_statut[1]}`, `gift_2 ${gift_statut[1]}`, `gift_3 ${gift_statut[1]}`]; // Array Gifts and add their statuts
+let fireplace_array = [`fireplace_0`, `fireplace_2`, `fireplace_3`, `fireplace_4`]; // Array Spawns
 let bonus; // Add a bonus
+//let gift_check; // Check gift statue
 let spawners_select; // Select the spawner
 let spawnTest;
 let turnInterval; // The periodic call to the turn function
@@ -31,6 +32,7 @@ let initialY;
 let move;
 let fireplace_select; //Choose the fireplace
 let laser; // Define the laser ID
+let health = 3; // Nb of life
 
 let speed = 3000; // Speed games
 let timer = 10;  //Game Timer
@@ -45,14 +47,14 @@ let timer_laser = 10; // Timer Laser
 
 //oxo.inputs.listenKey('enter', function() {
   // do something
-  oxo.screens.loadScreen('game', function() {
+  oxo.screens.loadScreen(`game`, function() {
     // game.html is loaded, do something
 
     //Select spawners
-    spawners_select = document.querySelectorAll('.game__area .spawner .spawner__position');
+    spawners_select = document.querySelectorAll(`.game__area .spawner .spawner__position`);
 
     //Select laser
-    laser = document.getElementById('laser');
+    laser = document.getElementById(`laser`);
     ///////////////////////////////////////////////////////////////////////////////////////////////
     //console.log('spawners_select  '+ spawners_select);
     ///////////////////////////////////////////////////////////////////////////////////////////////
@@ -77,11 +79,17 @@ function count() {
   timer--;
   if(timer == 0){
     finish();
+
+    // Load End game
+    /* 
+    oxo.screens.loadScreen('end', function() {
+    });
+    */
   } 
   else {
     // WHile time != 0 do :
     ///////////////////////////////////////////////////////////////////////////////////////////////
-    //console.log( timer + " secondes restantes");
+    console.log( timer + " secondes restantes");
     ///////////////////////////////////////////////////////////////////////////////////////////////
   }	
 }
@@ -117,11 +125,11 @@ function spawn() {
   //console.log('fireplace_select ' +fireplace_select);
 
   //Remove all classes
-  spawners_select[fireplace_select].classList.remove('spawner__position', 
-  'gift_0', 
-  'gift_1', 
-  'gift_2', 
-  'gift_3');
+  spawners_select[fireplace_select].classList.remove(`spawner__position`, 
+  `gift_0`, 
+  `gift_1`, 
+  `gift_2`, 
+  `gift_3`);
 
   //  Add a random gift and define 1 ID for each
   NbGifts++;
@@ -130,17 +138,17 @@ function spawn() {
   //  Select the last id was created
   character = document.getElementById(`gift_moving_${NbGifts}`);
   position = oxo.animation.getPosition(character);
+  
 
   ///////////////////////////////////////////////////////////////////////////////////////////////
   //console.log( spawners_select[fireplace_select]);
-  //console.log(character);
+  console.log(character);
   //console.log(position = oxo.animation.getPosition(character));
   //console.log(position.x);
   ///////////////////////////////////////////////////////////////////////////////////////////////
   
 }
 //
-
 
 /*
 * Keys write
@@ -150,42 +158,64 @@ function spawn() {
 * f : fireplace_3
 */
 
-oxo.inputs.listenKeys(['q', 's','d', 'f'], function(key) {
+oxo.inputs.listenKeys([ `q`, `s`, `d`, `f`], function(key) {
 
   // Visual Effect
-  laser.classList.add("laser");
+  laser.classList.add(`laser`);
   // Disapear laser after a delay
   setTimeout(function() {
-    laser.classList.remove("laser");
+    laser.classList.remove(`laser`);
   }, timer_laser);
 
-  if(key === 'q'){
-    if (position_minimum <= position.y  &&  position.y <= position_final) {
-      // Add to score
-      oxo.player.addToScore(5);
-    }
-  }
+  //Check if the gift status
+  if(character.className === `bad`){
+    health--;
+    speed = speed- 15;
 
-  if(key === 's'){
-    if (position_minimum <= position.y  &&  position.y <= position_final) {
-      // Add to score
-      oxo.player.addToScore(5);
-    }
-  }
+  ///////////////////////////////////////////////////////////////////////////////////////////////
+  ///console.log('bad');
+  ///////////////////////////////////////////////////////////////////////////////////////////////
 
-  if(key === 'd'){
-    if (position_minimum <= position.y  &&  position.y <= position_final) {
-      // Add to score
-      oxo.player.addToScore(5);
-    }
-  }
 
-  if(key === 'f'){
-    if (position_minimum <= position.y  &&  position.y <= position_final) {
-      // Add to score
-      oxo.player.addToScore(5);
+  } else {
+    //Check the gift and add point
+    if(key === `q`){
+      if (position_minimum <= position.y  &&  position.y <= position_final) {
+        // Add to score
+        oxo.player.addToScore(5);
+        speed = speed- 30;
+      }
+    }
+    
+    if(key === `s`){
+      if (position_minimum <= position.y  &&  position.y <= position_final) {
+        // Add to score 
+        oxo.player.addToScore(5);
+        speed = speed- 30;
+      }
+    }
+    
+    if(key === `d`){
+      if (position_minimum <= position.y  &&  position.y <= position_final) {
+        // Add to score
+        oxo.player.addToScore(5);
+        speed = speed- 30;
+      }
+    }
+    
+    if(key === `f`){ddd
+      if (position_minimum <= position.y  &&  position.y <= position_final) {
+        // Add to score
+        oxo.player.addToScore(5);
+        speed = speed- 30;
+      }
     }
   }
+  
+  ///////////////////////////////////////////////////////////////////////////////////////////////
+  //console.log('good');
+  //console.log(key);
+  ///////////////////////////////////////////////////////////////////////////////////////////////
 });
 //
 
